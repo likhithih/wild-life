6af8a6fbe754897b1a3d2bee62cc585a07b44768
likhithih <likhithih2003@gmail.com>
2025-11-06 10:34:20 +0530

    add Home page and Navbar component; update App to render Home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
 import Content from './components/Content'
+import Home from './pages/Home'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,7 +14,8 @@ function App() {
   <>
   {/* <Login/> */}
   {/* <Signup/> */}
-  <Content/>
+  {/* <Content/> */}
+  <Home/>
   </>
   )
 }
